39499ad045b7c04a309e9b8bed43adbcbaf84de8
Nicomeji1221 <nicolas.mejia1221@gmail.com>
2025-02-28 09:38:19 -0500

    CommitFinal

--- SamuelUribe-ValeryMonsalve/SamuelUribe-ValeryMonsalve/TestLiquidadorNomina.py
+++ SamuelUribe-ValeryMonsalve/SamuelUribe-ValeryMonsalve/TestLiquidadorNomina.py
@@ -1,7 +1,7 @@
 import unittest
 import Calculo_Total
 
-class TestLiquidadorNomi(unittest.TestCase):
+class TestLiquidadorNomina(unittest.TestCase):
 
     def test_normal_1(self):
         salario_base = 2000000
@@ -45,11 +45,12 @@ class TestLiquidadorNomi(unittest.TestCase):
     def test_extraordinario_4(self):
         salario_base = 1300000
         horas_diurnas = 36
-        horas_nocturnas = 55
+        horas_nocturnas = 15
         bonos_extra = 100000
         deduccion_adicional = 0
 
-        expected = 1723157.47
+        expected = 1552341.07
+
 
         result = Calculo_Total.Calculo_Total(salario_base, horas_diurnas, horas_nocturnas, bonos_extra, deduccion_adicional)
 
@@ -62,7 +63,7 @@ class TestLiquidadorNomi(unittest.TestCase):
         bonos_extra = 0
         deduccion_adicional = 500000
 
-        expected = 2949040
+        expected = 1949040
 
         result = Calculo_Total.Calculo_Total(salario_base, horas_diurnas, horas_nocturnas, bonos_extra, deduccion_adicional)
 
@@ -80,8 +81,58 @@ class TestLiquidadorNomi(unittest.TestCase):
         result = Calculo_Total.Calculo_Total(salario_base, horas_diurnas, horas_nocturnas, bonos_extra, deduccion_adicional)
 
         self.assertAlmostEqual(expected, result, 2)
+    
+    
+    
+    def test_error_1(self):
+        salario_base = -1500000
+        horas_diurnas = 0
+        horas_nocturnas = 0
+        bonos_extra = 0
+        deduccion_adicional = 0
 
+        with self.assertRaises (Calculo_Total.ErrorSalarioN):
+
+            result = Calculo_Total.Calculo_Total(salario_base, horas_diurnas, horas_nocturnas, bonos_extra, deduccion_adicional)
+
+
+    def test_error_2(self):
+        salario_base = 3500000
+        horas_diurnas = 12
+        horas_nocturnas = 12
+        bonos_extra = 300000
+        deduccion_adicional = 1400000
+                    
+        with self.assertRaises (Calculo_Total.ErrorDeduccionesM):
+
+             result = Calculo_Total.Calculo_Total(salario_base, horas_diurnas, horas_nocturnas, bonos_extra, deduccion_adicional)
+
+    
+        
+        
+    def test_error_3(self):
+        salario_base = 5000000
+        horas_diurnas = 43
+        horas_nocturnas = 47
+        bonos_extra = 0
+        deduccion_adicional = 0
+                    
+        with self.assertRaises (Calculo_Total.ErrorHorasExtra):
+            
+            result = Calculo_Total.Calculo_Total(salario_base, horas_diurnas, horas_nocturnas, bonos_extra, deduccion_adicional)
+
+    def test_error_4(self):
+        salario_base = "tres millones"
+        horas_diurnas = 0
+        horas_nocturnas = 0
+        bonos_extra = 0
+        deduccion_adicional = 0
+                    
+        with self.assertRaises (Exception):
+            
+            result = Calculo_Total.Calculo_Total(salario_base, horas_diurnas, horas_nocturnas, bonos_extra, deduccion_adicional)
+        
+        
 if __name__ == '__main__':
+    # print( Payment.calcularCuota.__doc__)
     unittest.main()
-
-#holaaa
---
+++ SamuelUribe-ValeryMonsalve/SamuelUribe-ValeryMonsalve/consola.py
@@ -0,0 +1,27 @@
+import Calculo_Total
+
+try:
+    
+    salario_base = float(input("Ingrese su salario base: "))
+    horas_diurnas = int(input("Ingrese sus horas extras diurnas: "))
+    horas_nocturnas = int(input("Ingrese sus horas extras nocturnas: "))
+    bonos_extra = float(input("Ingrese sus bonos extras: "))
+    deduccion_adicional = float(input("Ingrese sus deducciones adicionales: "))
+
+    nomina = Calculo_Total.Calculo_Total(salario_base, horas_diurnas, horas_nocturnas, bonos_extra, deduccion_adicional)
+
+    print (f"El valor total de su nomina es {nomina}")
+
+except Calculo_Total.ErrorSalarioN as ex:
+    print( str(ex))
+    
+except Calculo_Total.ErrorDeduccionesM as ex:
+    print( str(ex))
+
+  
+except Calculo_Total.ErrorHorasExtra as ex:
+    print( str(ex))
+    
+except Exception as ex:
+    print( str(ex))
+    print("¡Error digitacion! No puedes ingresar letras, por favor corrija ingresando datos numericos. ")
